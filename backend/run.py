d852747c26ef143fbdf35cac51e2429e0ee55d45
aryan017 <68499358+aryan017@users.noreply.github.com>
2025-01-11 22:28:05 +0530

    Add PORT

--- backend/run.py
+++ backend/run.py
@@ -3,4 +3,7 @@ from app import create_app
 app=create_app()
 
 if __name__=='__main__':
+    port = int(os.environ.get('PORT', 5000))
+    # Bind to 0.0.0.0 to make the app accessible externally
+    app.run(host='0.0.0.0', port=port)
     app.run(debug=True)
